74e641aa60cb6701c2cc6a98076b8f795ecd6038
James Bianchi <james.binchi@gmail.com>
2020-04-05 11:32:27 -0300

    ContaBancaria - Primeira Parte

---
+++ Conta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Conta
+    {
+        public int _conta ;
+        public string _titular;
+        public double Saldo;
+
+        public Conta()
+        {
+
+        }
+
+        public Conta(int conta, string titular, double valor)
+        {
+            _conta = conta;
+            _titular = titular;
+            Saldo = valor;
+        }
+
+        public override string ToString()
+        {
+            return "Conta Corrente: " + _conta
+                + " Nome do Titular: "+ _titular 
+                + " Saldo da Conta : R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture) ;
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CONTA BANCÁRIA");
+            Console.WriteLine("===============");
+            Console.WriteLine();
+
+            double valor = 0.00;
+
+            Console.Write("Digite o Número da Conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o Nome do Titular da Conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá Depósito Inicial: (s)/(n)");
+            string check = Console.ReadLine();
+            if (check == "s")
+            {
+                Console.Write("Entre com valor do Depósito Inicial: R$ ");
+                valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            
+            Conta con = new Conta(conta, titular, valor);
+
+            Console.WriteLine(con);
+                
+        }
+    }
+}
